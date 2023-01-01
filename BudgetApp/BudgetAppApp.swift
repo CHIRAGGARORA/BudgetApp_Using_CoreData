a8becd28d2ed0d505aa25aec2da704c2d822f003
Chirag Arora <arora2002chirag@gmail.com>
2023-01-01 11:55:44 +0530

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by chirag arora on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
